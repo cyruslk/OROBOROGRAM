a8b7ebb61476f09e09c893f89e7a43a5f09d37d0
cyruslk <cyrus7@hotmail.fr>
2018-03-28 15:05:36 -0400

    Adding the T. API

--- public/js/client.js
+++ public/js/client.js
@@ -1,75 +1,41 @@
 var socket = io();
 
 
-function scrollToBottom () {
-  // Selectors
-  var messages = jQuery('#messages');
-  var newMessage = messages.children('li:last-child')
-  var clientHeight = messages.prop('clientHeight');
-  var scrollTop = messages.prop('scrollTop');
-  var scrollHeight = messages.prop('scrollHeight');
-  var newMessageHeight = newMessage.innerHeight();
-  var lastMessageHeight = newMessage.prev().innerHeight();
-
-  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
-    messages.scrollTop(scrollHeight);
-  }
-}
-
-socket.on('connect', function () {
+socket.on('connect', function() {
     console.log("new content posted");
 });
 
-socket.on('disconnect', function () {
-  console.log('Disconnected from server');
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
 });
 
-socket.on('getTheUsersData', function (message) {
+socket.on('getTheUsersData', function(message) {
+
+    message: message
+    console.log(message);
+
+    let bodyObj = message.bodyObj;
+    let arrayNumbs = message.arrayNumbs;
+    let url = message.randomImg;
+
+    // let randomValue = randomValues[Math.floor(Math.random() * ((randomValues.length - 1) - 0 + 1))];
+    // console.log("HERE!" , randomValue);
+
+    let randomFilters = ["contrast", "grayscale", "invert", "opacity", "saturate", "sepia"];
+    let randomFilter = randomFilters[Math.floor(Math.random() * ((randomFilters.length - 1) - 0 + 1))];
 
 
-  console.log("this is the fucking data", message);
+    var template = jQuery("#message-template").html();
 
-  let arrayLength = message.bodyObj.data.children.length;
-  console.log("0", arrayLength);
-  let randomArraylength = Math.floor(Math.random() * Math.floor(arrayLength))
-  console.log("1", randomArraylength);
-  let title = message.bodyObj.data.children[randomArraylength].data.title;
-  let url = message.bodyObj.data.children[randomArraylength].data.url;
-  let num_comments = message.bodyObj.data.children[randomArraylength].data.num_comments;
-  let score = message.bodyObj.data.children[randomArraylength].data.score;
-  let created = message.bodyObj.data.children[randomArraylength].data.created;
-  let created_utc = message.bodyObj.data.children[randomArraylength].data.created_utc;
-  let downs = message.bodyObj.data.children[randomArraylength].data.downs;
-  let gilded = message.bodyObj.data.children[randomArraylength].data.gilded;
-  let thumbnail_height = message.bodyObj.data.children[randomArraylength].data.thumbnail_height;
-  let thumbnail_width =  message.bodyObj.data.children[randomArraylength].data.thumbnail_width;
+    var html = Mustache.render(template, {
+        url: url,
+        // randomFilter: randomFilter,
+        // randomValue: randomValue
+    });
 
+    // $('#message-template').html(html);
 
-  let randomValues = [num_comments, score, created, created_utc, downs, gilded, gilded, thumbnail_height, thumbnail_width];
-  let randomValue = randomValues[Math.floor(Math.random() * ((randomValues.length - 1) - 0 + 1))];
-  console.log("HERE!" , randomValue);
-
-  let randomFilters = ["contrast", "grayscale", "invert", "opacity", "saturate", "sepia"];
-  let randomFilter = randomFilters[Math.floor(Math.random() * ((randomFilters.length - 1) - 0 + 1))];
-
-
-  var template = jQuery("#message-template").html();
-
-  var html = Mustache.render(template, {
-    url: url,
-    title: title,
-    randomFilter: randomFilter,
-    randomValue: randomValue
-  });
-
-  // $('#message-template').html(html);
-
-  $('#target').html(html);
-
-
-
-
-  scrollToBottom();
+    $('#target').html(html);
 
 
 });
--- server/server.js
+++ server/server.js
@@ -14,48 +14,64 @@ var io = socketIO(server);
 var Jimp = require("jimp");
 
 var fs = require('fs');
-    Twit = require('twit');
+var Twit = require('twit')
+var T = new Twit(require('./config.js'))
+
 
 
 var options = {
-  renderDelay: 8000,
+    renderDelay: 8000,
 };
 
 
 io.on("connection", (socket) => {
-  console.log("New connection from the client!");
+    console.log("New connection from the client!");
 
-    request('https://www.reddit.com/r/foodporn.json', function (error, response, body) {
-      var bodyObj = JSON.parse(body);
-      // console.log("THIS SHIT", bodyObj);
+    request('https://www.reddit.com/r/foodporn.json', function(error, response, body) {
+        var bodyObj = JSON.parse(body);
 
-    socket.emit('getTheUsersData', {
-      bodyObj: bodyObj
+        let arrayLength = bodyObj.data.children.length;
+        var randomArraylength = Math.floor(Math.random() * Math.floor(arrayLength))
+        console.log(randomArraylength);
+
+        let ArrayOfThePickedEle = _.values(bodyObj.data.children[randomArraylength].data);
+
+        let arrayNumbs = ArrayOfThePickedEle.filter(ele => _.isNumber(ele));
+        console.log(arrayNumbs);
+        let randomImg = bodyObj.data.children[randomArraylength].data.url;
+        console.log(randomImg);
+
+        T.post('statuses/update', { status: `${randomImg}` }, function(err, data, response) {
+          console.log(data)
+        })
+
+        socket.emit('getTheUsersData', {
+            bodyObj: bodyObj,
+            arrayNumbs: arrayNumbs,
+            randomImg: randomImg
+        });
+
+        webshot(randomImg, 'food.jpg', options, function(err) {
+          console.log("what is this", randomImg);
+            Jimp.read("food.jpg", function(err, food) {
+                if (err) throw err;
+                food.color([
+                    { apply: 'hue', params: [ 10 ] },
+                    { apply: 'lighten', params: [ 1 ] },
+                    { apply: 'xor', params: [ '#06D' ] }
+                ]).write("food-small-bw.jpg"); // save
+
+
+            });
+
+        });
+
     });
 
 
-    let arrayLength = bodyObj.data.children.length;
-    console.log("0", arrayLength);
-    let randomArraylength = Math.floor(Math.random() * Math.floor(arrayLength))
-
-    var random_img = bodyObj.data.children[randomArraylength].data.url;
-
-    webshot(random_img, 'food.jpg', options, function(err) {
-
-      Jimp.read("food.jpg", function (err, food) {
-          if (err) throw err;
-          food.quality(100)                 // set JPEG quality
-               .write("food-small-bw.jpg"); // save
-      });
-
-    });
-
-  });
-
-
-  socket.on("disconnect", () => {
-    console.log("New disconnection from the client!");
-  })
+    socket.on("disconnect", () => {
+        console.log("New disconnection from the client!");
+    })
 })
 
 var data = require('fs').readFileSync('food-small-bw.jpg');
@@ -63,10 +79,40 @@ var data = require('fs').readFileSync('food-small-bw.jpg');
 app.use(express.static(publicPath));
 
 
-server.listen(port, () =>{
-  console.log(`Server is up on port ${port}`);
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 })
 
 
-
-  // VJm2CF0caRX62UfO5QQ8h9KWB
+// function upload_random_image(images){
+//   console.log('Opening an image...');
+//   var image_path = path.join(__dirname, '/images/' + random_from_array(images)),
+//       b64content = fs.readFileSync(image_path, { encoding: 'base64' });
+//
+//   console.log('Uploading an image...');
+//
+//   T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+//     if (err){
+//       console.log('ERROR:');
+//       console.log(err);
+//     }
+//     else{
+//       console.log('Image uploaded!');
+//       console.log('Now tweeting it...');
+//
+//       T.post('statuses/update', {
+//         media_ids: new Array(data.media_id_string)
+//       },
+//         function(err, data, response) {
+//           if (err){
+//             console.log('ERROR:');
+//             console.log(err);
+//           }
+//           else{
+//             console.log('Posted an image!');
+//           }
+//         }
+//       );
+//     }
+//   });
+// }
