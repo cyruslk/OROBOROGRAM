dbb0ca71fd3d4d4fe1be3c022ffb35fc91336c86
cyruslk <cyrus7@hotmail.fr>
2018-03-29 16:56:08 -0400

    Adding the T.post to display the altered images

--- public/js/client.js
+++ public/js/client.js
@@ -9,33 +9,33 @@ socket.on('disconnect', function() {
     console.log('Disconnected from server');
 });
 
-// socket.on('getTheUsersData', function(message) {
-//
-//     message: message
-//     console.log(message);
-//
-//     let bodyObj = message.bodyObj;
-//     let arrayNumbs = message.arrayNumbs;
-//     let url = message.randomImg;
-//
-//     // let randomValue = randomValues[Math.floor(Math.random() * ((randomValues.length - 1) - 0 + 1))];
-//     // console.log("HERE!" , randomValue);
-//
-//     let randomFilters = ["contrast", "grayscale", "invert", "opacity", "saturate", "sepia"];
-//     let randomFilter = randomFilters[Math.floor(Math.random() * ((randomFilters.length - 1) - 0 + 1))];
-//
-//
-//     var template = jQuery("#message-template").html();
-//
-//     var html = Mustache.render(template, {
-//         url: url,
-//         // randomFilter: randomFilter,
-//         // randomValue: randomValue
-//     });
-//
-//     // $('#message-template').html(html);
-//
-//     $('#target').html(html);
-//
-//
-// });
+socket.on('getTheUsersData', function(message) {
+
+    message: message
+    console.log(message);
+
+    let bodyObj = message.bodyObj;
+    let arrayNumbs = message.arrayNumbs;
+    let url = message.randomImg;
+
+    // let randomValue = randomValues[Math.floor(Math.random() * ((randomValues.length - 1) - 0 + 1))];
+    // console.log("HERE!" , randomValue);
+
+    let randomFilters = ["contrast", "grayscale", "invert", "opacity", "saturate", "sepia"];
+    let randomFilter = randomFilters[Math.floor(Math.random() * ((randomFilters.length - 1) - 0 + 1))];
+
+
+    var template = jQuery("#message-template").html();
+
+    var html = Mustache.render(template, {
+        url: url,
+        // randomFilter: randomFilter,
+        // randomValue: randomValue
+    });
+
+    // $('#message-template').html(html);
+
+    $('#target').html(html);
+
+
+});
--- server/server.js
+++ server/server.js
@@ -17,6 +17,11 @@ var fs = require('fs');
 var Twit = require('twit')
 var T = new Twit(require('./config.js'))
 
+var imageToSend = fs.readFileSync('food-small-bw.jpg', {encoding: 'base64'});
+
+console.log(imageToSend);
+
+
 
 
 var options = {
@@ -32,31 +37,54 @@ io.on("connection", (socket) => {
 
         let arrayLength = bodyObj.data.children.length;
         var randomArraylength = Math.floor(Math.random() * Math.floor(arrayLength))
-        console.log(randomArraylength);
-
         let ArrayOfThePickedEle = _.values(bodyObj.data.children[randomArraylength].data);
-
         let randomArrayTitle = bodyObj.data.children[randomArraylength].data.title;
-
         let arrayNumbs = ArrayOfThePickedEle.filter(ele => _.isNumber(ele));
-        console.log(arrayNumbs);
         let randomImg = bodyObj.data.children[randomArraylength].data.url;
-        console.log(randomImg);
 
-        T.post('statuses/update', { status: `${randomImg} - ${randomArrayTitle} - ${arrayNumbs}` }, function(err, data, response) {
-          // console.log(data)
 
-          socket.emit("browseTwitterArray", () => {
-              console.log("New disconnection from the client!");
+        // T.post('statuses/update', { status: `${randomImg} - ${randomArrayTitle} - ${arrayNumbs} #food #foodporn` }, function(err, data, response) {
+        //   if (!error) {
+        //      console.log(data, response);
+        //    }
+
+
+        T.post('media/upload', {media: imageToSend}, function(error, media, response) {
+
+          if (!error) {
+
+            // If successful, a media object will be returned.
+            console.log(media);
+
+            // Lets tweet it
+            var status = {
+              status: `${randomImg} - ${randomArrayTitle} - ${arrayNumbs}`,
+              media_ids: media.media_id_string // Pass the media id string
+            }
+
+            T.post('statuses/update', status, function(error, tweet, response) {
+              if (!error) {
+                console.log(tweet);
+              }
+            });
+
+          }
+        });
+
+
+          T.get('statuses/user_timeline', {screen_name	: 'food_analytics'}, function(err, data, response) {
+            var isArr = data.slice(-1)[0];
+
           })
 
-        })
 
         webshot(randomImg, 'food.jpg', options, function(err) {
-          console.log("what is this", randomImg);
+          // console.log("what is this", randomImg);
             Jimp.read("food.jpg", function(err, food) {
                 if (err) throw err;
-                food.color([
+                food
+                    .autocrop()
+                    .color([
                     { apply: 'hue', params: [ 10 ] },
                     { apply: 'lighten', params: [ 1 ] },
                     { apply: 'xor', params: [ '#06D' ] }
