056d5d6297f731384581f8efa869c1bc40dfcbe7
cyruslk <cyrus7@hotmail.fr>
2018-03-29 17:16:53 -0400

    Refactoring the code

--- server/server.js
+++ server/server.js
@@ -12,21 +12,11 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 var Jimp = require("jimp");
-
 var fs = require('fs');
 var Twit = require('twit')
 var T = new Twit(require('./config.js'))
-
 var imageToSend = fs.readFileSync('food-small-bw.jpg', {encoding: 'base64'});
-
-console.log(imageToSend);
-
-
-
-
-var options = {
-    renderDelay: 8000,
-};
+// console.log(imageToSend);
 
 
 io.on("connection", (socket) => {
@@ -42,26 +32,13 @@ io.on("connection", (socket) => {
         let arrayNumbs = ArrayOfThePickedEle.filter(ele => _.isNumber(ele));
         let randomImg = bodyObj.data.children[randomArraylength].data.url;
 
-
-        // T.post('statuses/update', { status: `${randomImg} - ${randomArrayTitle} - ${arrayNumbs} #food #foodporn` }, function(err, data, response) {
-        //   if (!error) {
-        //      console.log(data, response);
-        //    }
-
-
         T.post('media/upload', {media: imageToSend}, function(error, media, response) {
-
           if (!error) {
-
-            // If successful, a media object will be returned.
             console.log(media);
-
-            // Lets tweet it
             var status = {
               status: `${randomImg} - ${randomArrayTitle} - ${arrayNumbs}`,
               media_ids: media.media_id_string // Pass the media id string
             }
-
             T.post('statuses/update', status, function(error, tweet, response) {
               if (!error) {
                 console.log(tweet);
@@ -70,16 +47,10 @@ io.on("connection", (socket) => {
 
           }
         });
-
-
           T.get('statuses/user_timeline', {screen_name	: 'food_analytics'}, function(err, data, response) {
             var isArr = data.slice(-1)[0];
-
           })
-
-
         webshot(randomImg, 'food.jpg', options, function(err) {
-          // console.log("what is this", randomImg);
             Jimp.read("food.jpg", function(err, food) {
                 if (err) throw err;
                 food
@@ -89,21 +60,14 @@ io.on("connection", (socket) => {
                     { apply: 'lighten', params: [ 1 ] },
                     { apply: 'xor', params: [ '#06D' ] }
                 ]).write("food-small-bw.jpg"); // save
-
-
             });
-
         });
-
     });
-
-
     socket.on("disconnect", () => {
         console.log("New disconnection from the client!");
     })
 })
 
-var data = require('fs').readFileSync('food-small-bw.jpg');
 
 app.use(express.static(publicPath));
 
