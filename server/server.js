2b37dc0c649f095f6c1ae52ce70945a957e57214
cyruslk <cyrus7@hotmail.fr>
2018-04-03 17:50:03 -0400

    Nesting Twitter & Reddit and changing the code to only post once

--- server/server.js
+++ server/server.js
@@ -16,126 +16,94 @@ var Jimp = require("jimp");
 var fs = require('fs');
 var Twit = require('twit')
 var T = new Twit(require('./config.js'))
-// var imageToAlter = fs.readFileSync('food.jpg');
+    // var imageToAlter = fs.readFileSync('food.jpg');
 
 io.on("connection", (socket) => {
 
-  var imageToSend = fs.readFileSync('food-small.jpg', {encoding: 'base64'});
+    var imageToSend = fs.readFileSync('food-small.jpg', {
+        encoding: 'base64'
+    });
 
-  var options = { screen_name: 'ltsYourFoodPorn'};
+    var options = {
+        screen_name: 'ltsYourFoodPorn'
+    };
+    T.get('statuses/user_timeline', options, function(err, data) {
 
-  T.get('statuses/user_timeline', options , function(err, data) {
+        let arrayLengthTwitter = data.length;
+        var randomArraylengthTwitter = Math.floor(Math.random() * Math.floor(arrayLengthTwitter))
+        var twittedLink = data[randomArraylengthTwitter].entities.media[0].media_url_https;
+        console.log(twittedLink);
+        let ArrayOfThePickedEleUser = _.values(data[randomArraylengthTwitter].user);
+        //
+        let arrayNumbsTwitter = ArrayOfThePickedEleUser.filter(ele => _.isNumber(ele));
+        //
+        // function getRandomValueTwitNumbers() {
+        //     return arrayNumbsTwitter[Math.floor(Math.random() * arrayNumbsTwitter.length)];
+        // }
 
-      let arrayLengthTwitter = data.length;
-      var randomArraylengthTwitter = Math.floor(Math.random() * Math.floor(arrayLengthTwitter))
-      var twittedLink = data[randomArraylengthTwitter].entities.media[0].media_url_https;
-      console.log(twittedLink);
-      let ArrayOfThePickedEleUser = _.values(data[randomArraylengthTwitter].user);
-      let arrayNumbsTwitter = ArrayOfThePickedEleUser.filter(ele => _.isNumber(ele));
+        request('https://www.reddit.com/r/foodporn.json', function(error, response, body) {
+            var bodyObj = JSON.parse(body);
 
-      function getRandomValueTwitNumbers(){
-        return arrayNumbsTwitter[Math.floor(Math.random() * arrayNumbsTwitter.length)];
-      }
+            let arrayLength = bodyObj.data.children.length;
+            var randomArraylength = Math.floor(Math.random() * Math.floor(arrayLength))
+            let ArrayOfThePickedEle = _.values(bodyObj.data.children[randomArraylength].data);
+            let randomArrayTitle = bodyObj.data.children[randomArraylength].data.title;
+            let arrayNumbs = ArrayOfThePickedEle.filter(ele => _.isNumber(ele));
+            let randomImg = bodyObj.data.children[randomArraylength].data.url;
 
-      console.log(arrayNumbsTwitter);
-
-      Jimp.read(twittedLink, function(err, twittedLink) {
-          if (err) throw err;
-          twittedLink
-              // .blur(getRandomValueTwitNumbers())
-              .resize(600, 600)
-              .color([
-              { apply: 'hue', params: [ getRandomValueTwitNumbers() ] }
-          ]).write("food-small.jpg"); // save
-          console.log("randomImg modified");
-
-      });
-
-                T.get('statuses/user_timeline', {screen_name	: 'food_analytics'}, function(err, data, response) {
-                  var isArr = data.slice(-1)[0];
-                  // console.log(isArr);
-                })
+            console.log(arrayNumbs);
 
 
-T.post('media/upload', {
-    media: imageToSend
-}, function(error, media, response) {
-    if (!error) {
-        // console.log(media);
-        var status = {
-            status: `${randomImg} - ${randomArrayTitle} - ${arrayNumbs}`,
-            media_ids: media.media_id_string // Pass the media id string
-        }
-        T.post('statuses/update', status, function(error, tweet, response) {
-            if (!error) {
-                // console.log(tweet);
+            function getRandomValue() {
+                return arrayNumbs[Math.floor(Math.random() * arrayNumbs.length)];
             }
-        });
 
-    }
-});
-  })
+            // console.log(arrayNumbsTwitter);
 
+            Jimp.read(twittedLink, function(err, twittedLink) {
+                if (err) throw err;
+                twittedLink
+                    .blur(getRandomValue())
+                    .resize(600, 600)
+                    .color([
+                    { apply: 'hue', params: [ getRandomValue()]}
+                ]).write("food-small.jpg"); // save
+                console.log("The image is modified!");
 
-    console.log("New connection from the client!");
+                  T.post('media/upload', {
+                      media: imageToSend
+                  }, function(error, media, response) {
+                      if (!error) {
+                          // console.log(media);
+                          var status = {
+                              status: `${arrayNumbsTwitter}`,
+                              media_ids: media.media_id_string // Pass the media id string
+                          }
+                          T.post('statuses/update', status, function(error, tweet, response) {
+                              if (!error) {
+                                  console.log("The modified image is sent to Twitter");
+                              }
+                          });
+                      }
+                  });
 
-    request('https://www.reddit.com/r/foodporn.json', function(error, response, body) {
-        var bodyObj = JSON.parse(body);
-
-        let arrayLength = bodyObj.data.children.length;
-        var randomArraylength = Math.floor(Math.random() * Math.floor(arrayLength))
-        let ArrayOfThePickedEle = _.values(bodyObj.data.children[randomArraylength].data);
-        let randomArrayTitle = bodyObj.data.children[randomArraylength].data.title;
-        let arrayNumbs = ArrayOfThePickedEle.filter(ele => _.isNumber(ele));
-        let randomImg = bodyObj.data.children[randomArraylength].data.url;
-
-        function getRandomImg(){
-          return bodyObj.data.children[randomArraylength].data.url;
-        }
-
-        function getRandomValue(){
-          return arrayNumbs[Math.floor(Math.random() * arrayNumbs.length)];
-        }
-          Jimp.read(randomImg, function(err, randomImg) {
-              if (err) throw err;
-              randomImg
-                  .blur(0)
-                  .resize(600, 600)
-                  .color([
-                  { apply: 'lighten', params: [getRandomValue()] },
-                  { apply: 'hue', params: [ getRandomValue() ] }
-              ]).write("food-small.jpg"); // save
-              console.log("randomImg modified");
-
-          });
-
-
-        T.post('media/upload', {media: imageToSend}, function(error, media, response) {
-          if (!error) {
-            // console.log(media);
-            var status = {
-              status: `${randomImg} - ${randomArrayTitle} - ${arrayNumbs}`,
-              media_ids: media.media_id_string // Pass the media id string
-            }
-            T.post('statuses/update', status, function(error, tweet, response) {
-              if (!error) {
-                // console.log(tweet);
-              }
             });
 
-          }
+
+            T.get('statuses/user_timeline', {screen_name	: 'food_analytics'}, function(err, data, response) {
+              var isArr = data.slice(-1)[0];
+              // console.log(isArr);
+            })
+
+
         });
 
+    })
 
-          T.get('statuses/user_timeline', {screen_name	: 'food_analytics'}, function(err, data, response) {
-            var isArr = data.slice(-1)[0];
-            console.log(isArr);
-          })
-
-    });
     socket.on("disconnect", () => {
         console.log("New disconnection from the client!");
     })
+
 })
 
 
