caa9c8de3e074bbcf974d74861a9512c3d8de85d
cyruslk <cyrus7@hotmail.fr>
2018-04-02 22:56:17 -0400

    Adding the fs to pipe an image from the reddit request

--- public/js/client.js
+++ public/js/client.js
@@ -14,11 +14,11 @@ socket.on('sendLastItem', function(message) {
   console.log("this is the message");
 
 
-    // var template = jQuery("#message-template").html();
-    // var html = Mustache.render(template, {
-    //     url: url,
-    // });
-    // $('#target').html(html);
+    var template = jQuery("#message-template").html();
+    var html = Mustache.render(template, {
+        url: url,
+    });
+    $('#target').html(html);
 
 
 });
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const socketIO = require("socket.io")
 const http = require("http");
+const https = require("https");
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 var _ = require('lodash');
@@ -16,11 +17,16 @@ var fs = require('fs');
 var Twit = require('twit')
 var T = new Twit(require('./config.js'))
 var imageToSend = fs.readFileSync('food-small-bw.jpg', {encoding: 'base64'});
-// console.log(imageToSend);
+
+
+var file = fs.createWriteStream("file.jpg");
+var getThisFile = https.get("https://i.redd.it/vv8vao2f8gp01.jpg", function(response) {
+  response.pipe(file);
+});
 
 
 io.on("connection", (socket) => {
-    console.log("New connection from the client!");
+    // console.log("New connection from the client!");
 
     request('https://www.reddit.com/r/foodporn.json', function(error, response, body) {
         var bodyObj = JSON.parse(body);
@@ -31,17 +37,18 @@ io.on("connection", (socket) => {
         let randomArrayTitle = bodyObj.data.children[randomArraylength].data.title;
         let arrayNumbs = ArrayOfThePickedEle.filter(ele => _.isNumber(ele));
         let randomImg = bodyObj.data.children[randomArraylength].data.url;
+        console.log("here it is", randomImg);
 
         T.post('media/upload', {media: imageToSend}, function(error, media, response) {
           if (!error) {
-            console.log(media);
+            // console.log(media);
             var status = {
               status: `${randomImg} - ${randomArrayTitle} - ${arrayNumbs}`,
               media_ids: media.media_id_string // Pass the media id string
             }
             T.post('statuses/update', status, function(error, tweet, response) {
               if (!error) {
-                console.log(tweet);
+                // console.log(tweet);
               }
             });
 
@@ -49,12 +56,13 @@ io.on("connection", (socket) => {
         });
           T.get('statuses/user_timeline', {screen_name	: 'food_analytics'}, function(err, data, response) {
             var isArr = data.slice(-1)[0];
+            console.log(isArr);
           })
           webshot(randomImg, 'food.jpg', function(err) {
             Jimp.read("food.jpg", function(err, food) {
                 if (err) throw err;
                 food
-                    .autocrop()
+                    .resize(Jimp.AUTO, 250)
                     .color([
                     { apply: 'saturate', params: [ 433 ] },
                     { apply: 'lighten', params: [ 10 ] }
