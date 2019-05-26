41ef2e733ba8d08e85b18952594ab12a499da22c
cyrus <cyrus7@hotmail.fr>
2019-05-25 21:09:05 -0400

    switching to promises soon

--- server/server.js
+++ server/server.js
@@ -16,90 +16,33 @@ var Jimp = require("jimp");
 var fs = require('fs');
 var Twit = require('twit')
 var T = new Twit(require('./config.js'))
-    // var imageToAlter = fs.readFileSync('food.jpg');
 
-io.on("connection", (socket) => {
+const runTheBot = () => {
+  const arrayOfImages = [];
+  T.get('search/tweets', { q: 'images', count: 100 }, function(err, data, response) {
+      return data.statuses.map((ele, index) => {
+        if(ele.entities.media){
 
-    var imageToSend = fs.readFileSync('food-small.jpg', {
-        encoding: 'base64'
-    });
+          const arrayOfImages = [];
+          const elePath = ele.entities.media[0].media_url_https;
 
-    var options = {
-        screen_name: 'afp'
-    };
-    T.get('statuses/user_timeline', options, function(err, data) {
+          arrayOfImages.push({
+            imgSource: elePath,
+            userFollowers: ele.user.followers_count,
+            userLikes: ele.user.friends_count
+          })
+        }else{
+          return;
+        }
+      })
+  })
+}
 
-        let arrayLengthTwitter = data.length;
+const alterTheImage = (array) => {
+  console.log(array[0], "------here");
+}
 
-        var randomArraylengthTwitter = Math.floor(Math.random() * Math.floor(arrayLengthTwitter))
-        var twittedLink = data[randomArraylengthTwitter].entities.media[0].media_url_https;
-
-
-        let ArrayOfThePickedEleUser = _.values(data[randomArraylengthTwitter].user);
-        let arrayNumbsTwitter = ArrayOfThePickedEleUser.filter(ele => _.isNumber(ele));
-
-        request('https://www.reddit.com/r/food.json', function(error, response, body) {
-            var bodyObj = JSON.parse(body);
-            let arrayLength = bodyObj.data.children.length;
-            var randomArraylength = Math.floor(Math.random() * Math.floor(arrayLength))
-            let ArrayOfThePickedEle = _.values(bodyObj.data.children[randomArraylength].data);
-            let randomArrayTitle = bodyObj.data.children[randomArraylength].data.title;
-            let arrayNumbs = ArrayOfThePickedEle.filter(ele => _.isNumber(ele) && ele > 0);
-            let twittedLink2 = bodyObj.data.children[randomArraylength].data.url;
-
-
-            Jimp.read(twittedLink2, function(err, twittedLink2) {
-
-                const filters = ["hue", "saturate", "brighten"];
-                if (err) throw err;
-                twittedLink2
-                    .blur(getRandomEleFromArray(arrayNumbs))
-                    .resize(600, 600)
-                    .color([
-                    { apply: getRandomEleFromArray(filters),
-                      params: [ getRandomEleFromArray(arrayNumbs)]
-                    }
-                ]).write("food-small.jpg");
-
-                  T.post('media/upload', {
-                      media: imageToSend
-                  }, function(error, media, response) {
-                      if (!error) {
-                          var status = {
-                              status: `${arrayNumbsTwitter}`,
-                              media_ids: media.media_id_string // Pass the media id string
-                          }
-                          T.post('statuses/update', status, function(error, tweet, response) {
-                              if (!error) {
-                                  console.log("The modified image is sent to Twitter");
-                              }
-                          });
-                      }
-                  });
-            });
-
-
-            console.log(twittedLink);
-
-            function getRandomEleFromArray(array) {
-                return array[Math.floor(Math.random() * array.length)];
-            }
-
-
-            T.get('statuses/user_timeline', {screen_name	: 'food_analytics'}, function(err, data, response) {
-            })
-
-        });
-    })
-
-    socket.on("disconnect", () => {
-        console.log("New disconnection from the client!");
-    })
-
-})
-
-
-app.use(express.static(publicPath));
+runTheBot();
 
 
 server.listen(port, () => {
