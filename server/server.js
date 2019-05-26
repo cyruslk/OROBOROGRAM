c772932f908c6b142d096980de6a00108e519236
cyrus <cyrus7@hotmail.fr>
2019-05-25 23:01:55 -0400

    then, db?

--- server/server.js
+++ server/server.js
@@ -17,30 +17,81 @@ var fs = require('fs');
 var Twit = require('twit')
 var T = new Twit(require('./config.js'))
 
+
+const imagesData = [];
+const imageToTweet = fs.readFileSync('altered-img.jpg', {
+    encoding: 'base64'
+});
+
 const runTheBot = () => {
   const arrayOfImages = [];
-  T.get('search/tweets', { q: 'images', count: 100 }, function(err, data, response) {
-      return data.statuses.map((ele, index) => {
+  T.get('search/tweets', { q: 'images', count: 100 })
+    .catch(function (err) {
+      console.log('caught error', err.stack)
+    })
+    .then(function (result) {
+
+      const data = result.data.statuses;
+
+      return data.map((ele, index) => {
         if(ele.entities.media){
+           const elePath = ele.entities.media[0].media_url_https;
+            return imagesData.push({
+              elePath: elePath,
+              userFollowers: ele.user.followers_count,
+              userLikes: ele.user.friends_count
+            })
+         }else{
+           return;
+         }
+      })
+    })
+    .then(() => {
+      let selectedEle =  imagesData[Math.floor(imagesData.length * Math.random())];
+      console.log(selectedEle);
 
-          const arrayOfImages = [];
-          const elePath = ele.entities.media[0].media_url_https;
-
-          arrayOfImages.push({
-            imgSource: elePath,
-            userFollowers: ele.user.followers_count,
-            userLikes: ele.user.friends_count
-          })
-        }else{
-          return;
-        }
+      Jimp.read(selectedEle.elePath)
+      .then(img => {
+        return img
+        .blur(selectedEle.userFollowers)
+        .color([{ apply: 'hue', params: [ selectedEle.userLikes]}])
+        .resize(600, 600)
+          .write('altered-img.jpg');
+      })
+      .catch(err => {
+        console.error(err);
+      })
+      .then(() => {
+        return tweetTheModifiedImage(selectedEle);
       })
   })
 }
 
-const alterTheImage = (array) => {
-  console.log(array[0], "------here");
-}
+
+
+tweetTheModifiedImage = (selectedEle) => {
+
+  T.post('media/upload', {
+        media: imageToTweet
+    }, function(error, media, response) {
+        if(error){
+          console.log(error);
+        }
+        if (!error) {
+            var status = {
+                status: `${selectedEle.userFollowers}, ${selectedEle.userLikes}`,
+                media_ids: media.media_id_string // Pass the media id string
+            }
+            T.post('statuses/update', status, function(error, tweet, response) {
+                if (!error) {
+                    console.log("Posted.");
+                }
+            });
+        }
+    });
+  }
+
+
 
 runTheBot();
 
